a01b90dd1a60f4bb22675e5b46c4ef4e56156870
misrudin <misrudinz@gmail.com>
2022-07-17 03:45:59 +0700

    test

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
