bdff0a233c666b10c6685b18292bd31c7508f8aa
misrudin <misrudinz@gmail.com>
2022-07-22 00:53:14 +0700

    api

--- models/analisis.js
+++ models/analisis.js
@@ -1,4 +1,4 @@
-const db = require('../configs/db');
+const db = require("../configs/db");
 
 module.exports = {
   getDataNilaiMahasiswa: () => {
@@ -14,21 +14,57 @@ module.exports = {
   },
   getDataCluster: (nim) => {
     return new Promise((resolve, reject) => {
-      db.query(`SELECT *
-                FROM view_data_nilai
-                where nim = ${nim}`, (err, result) => {
-        if (!err) {
-          if (result.length !== 0) {
-            const dataResult = result[0];
-            dataResult.data_nilai = JSON.parse(dataResult.data_nilai)
-            resolve(dataResult);
+      db.query(
+        `SELECT *
+         FROM view_data_nilai
+         where nim = ${nim}`,
+        (err, result) => {
+          if (!err) {
+            if (result.length !== 0) {
+              try {
+                const dataResult = result[0];
+                dataResult.data_nilai = JSON.parse(dataResult.data_nilai);
+                resolve(dataResult);
+              } catch (e) {
+                reject(new Error(`Data cluster ${nim} tidak ditemukan`));
+              }
+            } else {
+              reject(new Error(`Data cluster ${nim} tidak ditemukan`));
+            }
           } else {
-            reject(new Error(`Data cluster ${nim} tidak ditemukan`));
+            reject(new Error(err));
           }
-        } else {
-          reject(new Error(err));
         }
-      });
+      );
     });
-  }
-}
+  },
+  saveResult: (data) => {
+    return new Promise((resolve, reject) => {
+      const valuesInsert = data.map((item) => [item.nim, item.nama, item.cluster])
+      const sql = `INSERT INTO hasil (hasil.nim, hasil.nama, hasil.cluster) values ? ON DUPLICATE KEY UPDATE nim = VALUES(hasil.nim), nama = VALUES(hasil.nama), cluster = VALUES(hasil.cluster)`;
+      db.query(sql, [valuesInsert],
+        (err, result) => {
+          if (!err) {
+            resolve(data)
+          } else {
+            reject(new Error(err));
+          }
+        }
+      );
+    });
+  },
+  getLaporanAnalisis: () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("Suuccess");
+      }, 5000);
+    });
+  },
+  getTrenBidangSkripsi: () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("Suuccess");
+      }, 5000);
+    });
+  },
+};
--- routers/analisis.js
+++ routers/analisis.js
@@ -2,6 +2,9 @@ const express = require("express");
 const Router = express.Router();
 const controller = require("../controller/analisis");
 
-Router.use("/", controller.getAnalisis);
+Router.get("/", controller.getAnalisis);
+Router.get("/laporan-bidang-skripsi", controller.getLaporanAnalisis);
+Router.get("/tren-bidang-skripsi", controller.getTrenBidangSkripsi);
+Router.post("/save-result", controller.saveResult);
 
 module.exports = Router;
